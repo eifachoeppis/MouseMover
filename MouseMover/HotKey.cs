07eaab6486d9f14b77cbccdc3ca90e2bd8463c5c
Lukas Huber <lukas.huber@icr.ch>
2020-04-22 16:11:03 +0200

    Add project files.

---
+++ MouseMover/HotKey.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Interop;
+
+namespace MouseMover
+{
+    public sealed class HotKey : IDisposable
+    {
+        private static Dictionary<Int32, HotKey> registeredKeys;
+        private static IMessageFilter messageFilter;
+
+        static HotKey()
+        {
+            registeredKeys = new Dictionary<Int32, HotKey>();
+            messageFilter = new MessageFilter();
+        }
+
+        [DllImport("user32.dll")]
+        private static extern Boolean RegisterHotKey(IntPtr hWnd, Int32 id, UInt32 fsModifiers, UInt32 vlc);
+
+        [DllImport("user32.dll")]
+        private static extern Boolean UnregisterHotKey(IntPtr hWnd, Int32 id);
+
+        public const Int32 WmHotKey = 0x0312;
+
+        public Key Key { get; }
+        public KeyModifier KeyModifiers { get; }
+        public Action Action { get; }
+
+        private Int32 id;
+
+        public HotKey(Key k, KeyModifier keyModifiers, Action action)
+        {
+            this.Key = k;
+            this.KeyModifiers = keyModifiers;
+            this.Action = action ?? throw new ArgumentNullException(nameof(action));
+            Application.AddMessageFilter(messageFilter);
+            this.Register();
+        }
+
+        private void Register()
+        {
+            Int32 virtualKeyCode = KeyInterop.VirtualKeyFromKey(this.Key);
+            this.id = virtualKeyCode + ((Int32)this.KeyModifiers * 0x10000);
+            RegisterHotKey(IntPtr.Zero, this.id, (UInt32)this.KeyModifiers, (UInt32)virtualKeyCode);
+            registeredKeys.Add(this.id, this);
+        }
+
+        private void Unregister()
+        {
+            UnregisterHotKey(IntPtr.Zero, this.id);
+            registeredKeys.Remove(this.id);
+        }
+
+        public void Dispose()
+        {
+            this.Unregister();
+        }
+
+        private class MessageFilter : IMessageFilter
+        {
+            public Boolean PreFilterMessage(ref Message m)
+            {
+                if (m.Msg == WmHotKey)
+                {
+                    if (registeredKeys.TryGetValue((Int32)m.WParam, out HotKey hotKey))
+                    {
+                        hotKey.Action.Invoke();
+                        return true;
+                    }
+                }
+                return false;
+            }
+        }
+    }
+}
---
+++ MouseMover/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+using MouseMover.Properties;
+
+namespace MouseMover
+{
+    static class Program
+    {
+        private const Int32 STEPS = 3;
+        private const Int32 WIDTH = 1920;
+        private const Int32 HEIGHT = 1200;
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MouseMoverApplication());
+        }
+
+        public class MouseMoverApplication : ApplicationContext
+        {
+            private readonly NotifyIcon trayIcon;
+            private readonly HotKey startKey;
+            private readonly HotKey stopKey;
+            private CancellationTokenSource tokenSource;
+            private Task task;
+
+            public MouseMoverApplication()
+            {
+                MenuItem[] menuItems = new MenuItem[]
+                {
+                    new MenuItem("Exit", this.Exit)
+                };
+                this.trayIcon = new NotifyIcon
+                {
+                    Icon = Resources.Icon,
+                    ContextMenu = new ContextMenu(menuItems),
+                    Visible = true
+                };
+                this.startKey = new HotKey(Key.D, KeyModifier.Ctrl | KeyModifier.Alt, this.MoveCursor);
+                this.stopKey = new HotKey(Key.X, KeyModifier.Ctrl | KeyModifier.Alt, this.Cancel);
+            }
+
+            private void MoveCursor()
+            {
+                if (task == null || task.IsCompleted)
+                {
+                    this.tokenSource?.Dispose();
+                    this.tokenSource = new CancellationTokenSource();
+                    task = Task.Factory.StartNew(() =>
+                    {
+                        for (int i = 0; i < WIDTH; i++)
+                        {
+                            for (int j = 0; j < HEIGHT; j += HEIGHT - 1)
+                            {
+                                if (this.tokenSource.Token.IsCancellationRequested)
+                                {
+                                    return;
+                                }
+                                Thread.Sleep(10);
+                                Cursor.Position = new Point(i, j);
+                            }
+                        };
+                    }, this.tokenSource.Token, TaskCreationOptions.LongRunning, TaskScheduler.Current);
+                }
+            }
+
+            private void Cancel()
+            {
+                this.tokenSource?.Cancel();
+            }
+
+            private void Exit(object sender, EventArgs e)
+            {
+                this.Exit();
+            }
+
+            private void Exit()
+            {
+                this.tokenSource?.Cancel();
+                this.tokenSource?.Dispose();
+                this.startKey.Dispose();
+                this.stopKey.Dispose();
+                this.trayIcon.Dispose();
+                Application.Exit();
+            }
+        }
+    }
+}
